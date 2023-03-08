09ec105431324a0db3554d5ce1e39523cdb2fad6
Isabel <isabel@chaparian.no>
2023-03-08 16:33:00 +0100

    Added projects

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace BuberBreakfast.Contracts.Breakfast;
+
+public record BreakfastResponse
+(
+    Guid Id,
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    DateTime LastModifiedDate,
+    List<string> Savory,
+    List<string> Sweet
+);
---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,33 @@
+﻿namespace BuberBreakfast.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using BuberBreakfast.Contracts.Breakfast;
+
+[ApiController]
+public class BreakfastController: ControllerBase
+{
+    [HttpPost("/breakfasts")]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("/breakfasts/{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("/breakfasts/{id:guid}")]
+    public IActionResult UpdateBreakfast(UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("/breakfasts/{id:guid}")]
+    public IActionResult DeleteBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok();
+    }
+}
+
